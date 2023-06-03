ff18e070e66b6f48dd09d7a40cc88697f389a77a
wegamekinglc <scrappedprince.li@gmail.com>
2023-06-03 21:53:56 +0800

    change folder

---
+++ python/arrow_ex/example_02/memory_map.py
@@ -0,0 +1,5 @@
+import pyarrow as pa
+from arrow_ex.utils import generate_data
+
+if __name__ == "__main__":
+    pass
---
+++ python/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='arrow-ex',
+    version='1.0.0',
+    packages=['arrow_ex', 'arrow_ex.example_01', 'arrow_ex.example_02'],
+    url='',
+    license='',
+    author='wegam',
+    author_email='',
+    description=''
+)
